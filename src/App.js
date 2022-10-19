dbd22fceee9e77c7f51d39741b55706699e0d384
Ramona Snider <41329897+rlsnider@users.noreply.github.com>
2022-10-19 01:05:04 -0700

    played around with bootstrap

--- src/App.js
+++ src/App.js
@@ -1,13 +1,71 @@
 import './App.css';
-
+import Button from 'react-bootstrap/Button';
+import Nav from 'react-bootstrap/Nav';
+import Form from 'react-bootstrap/Form';
 
 
 function App() {
   return (
+    <div>
+    <Nav 
+      className="justify-content-center"
+      variant="tabs" defaultActiveKey="/home"
+      onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
+    >
+      <Nav.Item>
+        <Nav.Link href="/home">Active</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="link-1">Link</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="link-2">Link</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="disabled" disabled>
+          Disabled
+        </Nav.Link>
+      </Nav.Item>
+    </Nav>
+    
     <div className="App">
-      <h1>Bootstrap Website</h1>
-     
-    </div>
+      <h1>Hello World</h1>
+      <div>
+        <Button variant="primary" size="lg">Large Primary Button</Button>
+      </div>
+     </div>
+    
+    <Form>
+    <Form.Group className="mb-3" controlId="formBasicEmail">
+      <Form.Label>Email address</Form.Label>
+      <Form.Control type="email" placeholder="Enter email" />
+      <Form.Text className="text-muted">
+        We'll never share your email with anyone else.
+      </Form.Text>
+    </Form.Group>
+
+    <Form.Group className="mb-3" controlId="formBasicPassword">
+      <Form.Label>Password</Form.Label>
+      <Form.Control type="password" placeholder="Password" />
+    </Form.Group>
+    <Form.Group className="mb-3" controlId="formBasicCheckbox">
+      <Form.Check type="checkbox" label="Check me out" />
+    </Form.Group>
+    <Form.Group>
+      <Form.Label>From Dropdown with favorite movies</Form.Label>
+      <Form.Control as="select">
+        <option>Inception</option>
+        <option>Forrest Gump</option>
+        <option>Shawshank Redemption</option>
+        <option>Spiderman: Into the Spiderverse</option>
+        <option>Avengers: Endgame</option>
+      </Form.Control>
+    </Form.Group>
+    <Button variant="primary" type="submit">
+      Submit
+    </Button>
+  </Form>
+  </div>
   );
 }
 
